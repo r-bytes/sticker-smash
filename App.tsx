8ee9b0959206fccfe179b861b7097e069b551885
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-01-24 13:35:30 +0100

    build: project init

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar } from "expo-status-bar"
+import { Image, StyleSheet, View } from "react-native"
+import ImageViewer from "./components/ImageViewer"
+
+const placeholderImage = require("./assets/images/background-image.png")
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imageSource={placeholderImage} />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+})
