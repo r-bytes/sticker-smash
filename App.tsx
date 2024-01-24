818704d45a6e61aed2b10a62a5ab470001b92604
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-01-24 15:55:06 +0100

    pressable image picker

--- App.tsx
+++ App.tsx
@@ -1,15 +1,39 @@
+import * as ImagePicker from "expo-image-picker"
 import { StatusBar } from "expo-status-bar"
-import { Image, StyleSheet, View } from "react-native"
-import ImageViewer from "./components/ImageViewer"
+import { StyleSheet, View } from "react-native"
+import { Button, ImageViewer } from "./components/"
 
 const placeholderImage = require("./assets/images/background-image.png")
 
 export default function App() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    })
+
+    if (!result.canceled) {
+      console.log(result)
+    } else {
+      alert("You did not select any image.")
+    }
+  }
   return (
     <View style={styles.container}>
+      
       <View style={styles.imageContainer}>
         <ImageViewer imageSource={placeholderImage} />
       </View>
+
+      <View style={styles.footerContainer}>
+        <Button
+          label="Choose a photo"
+          theme="primary"
+          onPress={pickImageAsync}
+        />
+        <Button label="Use this photo" />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   )
@@ -25,5 +49,9 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 })
