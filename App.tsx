6b77a3096edd381c53329c581f0d49bf8af70e06
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-01-30 13:46:59 +0100

    finished project

--- App.tsx
+++ App.tsx
@@ -112,7 +112,7 @@ export default function App() {
         {/* A list of emoji component will go here */}
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </GestureHandlerRootView>
   )
 }
